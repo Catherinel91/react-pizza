6c57f7f0b505a681ae8aaa678614e827b6f4f181
Kate <199111k@gmail.com>
2022-09-25 14:17:22 +0300

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import cl from './example.module.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className={cl.qw}>init</h1>
+    </div>
+  );
+}
+
+export default App;
